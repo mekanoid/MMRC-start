ecb1e691757516bc27df5ab02ba7903ea1e4ccd6
Peter Kindström <mekanoid@infolagret.se>
2019-12-01 09:56:27 +0100

    Namnändrat filer

---
+++ mmrcStart/mmrcSettings.h
@@ -0,0 +1,16 @@
+/**
+  * Settings for this specific MMRC client
+  */
+// Access point
+#define APNAME "MMRC-start"
+#define APPASSWORD "mmrc1234"
+
+// Node one settings
+#define NODE01ID "button"
+#define NODE01TYPE "button"
+#define NODE01PROP01 "main"
+#define NODE01PROP01DATATYPE "string"
+#define NODE01PROP02 "slave"
+#define NODE01PROP02DATATYPE "string"
+#define SPECIAL01 "5"
+#define SPECIAL02 "10"
